99da29295f6194a44c7cd887c2c7c6cea47b2c2c
Radoslaw Kozlowski <radoslaw.kozlowski@ethisys.co.uk>
2020-07-13 12:36:01 +0200

    Disable payload

--- src/helix-check.js
+++ src/helix-check.js
@@ -50,8 +50,8 @@ async function check() {
         core.setOutput('result', result);
 
         // Get the JSON webhook payload for the event that triggered the workflow
-        const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
+        //const payload = JSON.stringify(github.context.payload, undefined, 2)
+        //console.log(`The event payload: ${payload}`);
 
     } catch (error) {
         core.setFailed(error.message);
